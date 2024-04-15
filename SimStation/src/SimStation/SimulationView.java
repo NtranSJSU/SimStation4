73d96ef8293d6f61b431ddd1bba1b49f5c325a3d
rayhannK <151211511+rayhannK@users.noreply.github.com>
2024-04-15 16:01:30 -0700

    Update SimulationView.java

--- SimStation/src/SimStation/SimulationView.java
+++ SimStation/src/SimStation/SimulationView.java
@@ -4,25 +4,38 @@ import mvc.Model;
 import mvc.View;
 
 import java.awt.*;
+import java.util.List;
 
 public class SimulationView extends View {
+
+    private List<Agent> agentList;
+
     public SimulationView(Model model) {
         super(model);
+        //the stuff within the constructor declares the casting and shit
+        //this way the other methods (not only paintcomponent) has access to the list of stuff
+        //do the same thing as line 14 in gridView
+        Simulation sim = (Simulation)model;
+        agentList = sim.getAgents();
     }
 
     public void paintComponent(Graphics gc) {
         super.paintComponent(gc);
-        gc.setColor(Color.WHITE);
-        //for loop through the getNeighbors...
+        gc.setColor(Color.LIGHT_GRAY);
+        for (Agent agent : agentList) {
+            gc.fillOval(agent.xcoord, agent.ycoord, 1, 1);
+            repaint();
+        }
+        //for loop through the getNeighbors...?
             //gc.fillOval(agent.xc, agent.yc, 1, 1)
+            //just call repaint() method, so that it repaints EVERYTHING just to move a few agents
+            //it's not very efficient, but it's whatever i just want it to work
+
         update();
 
     }
 
     public void update() {
         repaint();
-        //don't forget parameters:
-            //agent xCoord, agent yCoord, bounded area (i.e. what it should cover)
-                //i.e. bounded area = height & width
     }
 }
