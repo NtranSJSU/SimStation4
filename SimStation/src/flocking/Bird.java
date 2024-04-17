168c77e7a75f434c1cfe45866a6c615f9815680f
rayhannK <151211511+rayhannK@users.noreply.github.com>
2024-04-17 11:57:29 -0700

    Update Bird.java

--- SimStation/src/flocking/Bird.java
+++ SimStation/src/flocking/Bird.java
@@ -1,44 +1,32 @@
 package simstation.flocking;
 
 import simstation.Agent;
+import simstation.Heading;
 import simstation.Simulation;
-import simstation.SimulationPanel;
-import mvc.AppPanel;
 import mvc.Utilities;
 
-public class FlockingSimulation extends Simulation {
+public class Bird extends Agent {
+    public int speed;
 
-    public void populate() {
-        for (int i = 0; i < 15; i++) {
-            addAgent(new Bird());
-        }
+    public Bird() {
+        super();
+        heading = Heading.random();
+        speed = Utilities.rng.nextInt(5);
     }
 
     @Override
-    public void statistic() {
-        int s1, s2, s3, s4, s5;
-        s1 = s2 = s3 = s4 = s5 = 0;
-        for (int i = 0; i < agents.size(); i++) {
-            Agent agent = agents.get(i);
-            Bird bird = (Bird) agent;
-            if (bird.speed == 1) s1++;
-            if (bird.speed == 2) s2++;
-            if (bird.speed == 3) s3++;
-            if (bird.speed == 4) s4++;
-            if (bird.speed == 5) s5++;
+    public void update() {
+        Simulation world = getWorld();
+        Agent neighbor = world.getNeighbor(this, 15);
+        if (neighbor != null) {
+            Bird b = (Bird) neighbor;
+            int steps = b.speed;
+            heading = b.heading;
+            speed = b.speed;
+            move(steps);
         }
-        String[] birdSpeed = {
-                "#birds @ speed 1 = " + s1,
-                "#birds @ speed 2 = " + s2,
-                "#birds @ speed 3 = " + s3,
-                "#birds @ speed 4 = " + s4,
-                "#birds @ speed 5 = " + s5
-        };
-        Utilities.inform(birdSpeed);
-    }
-
-    public static void main(String[] args) {
-        AppPanel panel = new SimulationPanel(new FlockingFactory());
-        panel.display();
+        else {
+            move(speed);
+        }
     }
 }
