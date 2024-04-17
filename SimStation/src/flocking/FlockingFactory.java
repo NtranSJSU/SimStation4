03bd4df7fdb95df266d4e928848b9071ce7009b9
rayhannK <151211511+rayhannK@users.noreply.github.com>
2024-04-17 11:58:17 -0700

    Update FlockingFactory.java

--- SimStation/src/flocking/FlockingFactory.java
+++ SimStation/src/flocking/FlockingFactory.java
@@ -1,13 +1,12 @@
-package SimStation.flocking;
+package simstation.flocking;
 
-import SimStation.SimulationFactory;
+import simstation.SimulationFactory;
 import mvc.Model;
 
 public class FlockingFactory extends SimulationFactory {
     @Override
     public Model makeModel() {
         return new FlockingSimulation();
-        //returns new flocking model (like society)
     }
 
     @Override
