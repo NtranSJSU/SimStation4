441badfa1647ffff6a30d2e8c1713a39eb1cc03d
Max Zlochevsky <99788435+maxz2001@users.noreply.github.com>
2024-04-17 13:01:24 -0700

    Update PlagueFactory.java

--- SimStation/src/plague/PlagueFactory.java
+++ SimStation/src/plague/PlagueFactory.java
@@ -1,46 +1,18 @@
 package plague;
 
-import mvc.AppFactory;
-import mvc.Command;
+import simstation.SimulationFactory;
 import mvc.Model;
-import simstation.StatsCommand;
-import plague.PlagueStatsCommand;
 
-public class PlagueFactory implements AppFactory {
-
+public class PlagueFactory extends SimulationFactory {
     @Override
     public Model makeModel() {
         return new PlagueSimulation();
     }
 
     @Override
-    public mvc.View makeView(Model model) {
-        return new simstation.SimulationView(model);
+    public String getTitle() { //go to line 50
+        return "Plague Simulator";
     }
 
-    @Override
-    public String getTitle() {
-        return "Plague Simulation";
-    }
-
-    @Override
-    public String[] getHelp() {
-        return new String[]{"Plague Simulation Help Information"};
-    }
-
-    @Override
-    public String about() {
-        return "About Plague Simulation";
-    }
-
-    @Override
-    public String[] getEditCommands() {
-        return new String[]{"Stats"};
-    }
-
-    @Override
-    public Command makeEditCommand(Model model, String type, Object source) {
-        if (type.equals("Stats")) return new PlagueStatsCommand((PlagueSimulation) model);
-        return null;
-    }
+    public String about() { return "Plague Simulator "; }
 }
