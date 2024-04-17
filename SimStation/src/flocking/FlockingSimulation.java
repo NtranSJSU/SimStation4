d78d2d909482096dee7bc35e82d2d8eaa37f8946
rayhannK <151211511+rayhannK@users.noreply.github.com>
2024-04-17 11:56:38 -0700

    Update FlockingSimulation.java

--- SimStation/src/flocking/FlockingSimulation.java
+++ SimStation/src/flocking/FlockingSimulation.java
@@ -1,8 +1,8 @@
-package SimStation.flocking;
+package simstation.flocking;
 
-import SimStation.Agent;
-import SimStation.Simulation;
-import SimStation.SimulationPanel;
+import simstation.Agent;
+import simstation.Simulation;
+import simstation.SimulationPanel;
 import mvc.AppPanel;
 import mvc.Utilities;
 
@@ -16,13 +16,25 @@ public class FlockingSimulation extends Simulation {
 
     @Override
     public void statistic() {
-        String[] birds = new String[agents.size()];
+        int s1, s2, s3, s4, s5;
+        s1 = s2 = s3 = s4 = s5 = 0;
         for (int i = 0; i < agents.size(); i++) {
             Agent agent = agents.get(i);
             Bird bird = (Bird) agent;
-            birds[i] = "#birds @ speed " + (i + 1) + " = " + bird.speed;
+            if (bird.speed == 1) s1++;
+            if (bird.speed == 2) s2++;
+            if (bird.speed == 3) s3++;
+            if (bird.speed == 4) s4++;
+            if (bird.speed == 5) s5++;
         }
-        Utilities.inform(birds);
+        String[] birdSpeed = {
+                "#birds @ speed 1 = " + s1,
+                "#birds @ speed 2 = " + s2,
+                "#birds @ speed 3 = " + s3,
+                "#birds @ speed 4 = " + s4,
+                "#birds @ speed 5 = " + s5
+        };
+        Utilities.inform(birdSpeed);
     }
 
     public static void main(String[] args) {
